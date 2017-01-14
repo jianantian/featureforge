f7c8e27132e066e573b1f2755ec3b4dc271a939c
jianantian <emile.zhu@hotmail.com>
2017-01-14 08:40:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 
 base_path = os.path.dirname(os.path.abspath(__file__))
-long_description = open(os.path.join(base_path, 'README.rst')).read()
+long_description = open(os.path.join(base_path, 'README.rst'), encoding='utf8').read()
 
 setup(
     name="featureforge",
